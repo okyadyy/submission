e3ef65022635ab588d67eddbdb059df41e64b653
okyadyy <okyadyyy@gmail.com>
2023-08-14 13:51:43 +0700

    extending main layout

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/post', function () {
+Route::get('/form', function () {
     return view('form');
 });
