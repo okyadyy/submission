c153d7cabef65ebc9f8945b7a00fd210b8d51570
okyadyy <okyadyyy@gmail.com>
2023-08-15 16:42:26 +0700

    display data on card section & rename relation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Card Section -->
-    <x-card-section></x-card-section>
+    <x-card-section :cardvalue="$cardvalue" :recenthistories="$recenthistories"></x-card-section>
 
     <x-table-section :records="$records"></x-table-section>
 @endsection
